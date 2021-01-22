175cbe8b40ee5ee643fe68ac228afbe3bc83717f
rsyd29 <brasyid15@gmail.com>
2021-01-22 15:01:08 +0700

    update say hello with parameter name video 65

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/rsyd29/go-say-hello
+module github.com/rsyd29/go-say-hello/v2
 
 go 1.15
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,8 @@
 package go_say_hello
 
-func SayHello() string { // jangan lupa ini uppercase agar public
-	return "Hello, I'm Budiman Rasyid\nBerikut adalah code terbarunya"
+// kita ubah func SayHello agar memiliki parameter, agar codingannya rusak
+// karena orang yang memanggil func SayHello itu error semua, karena func tersebut diwajibkan memasukkan parameter
+// sebelumnya tidak menggunakan parameter name
+func SayHello(name string) string { // jangan lupa ini uppercase agar public
+	return "Hello, I'm"+ name + "\nBerikut adalah code terbarunya"
 }
